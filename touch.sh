fbdcf42e4e94159dbba74d20fc86e757f171f66f
yedotimahesh <122370730+yedotimahesh@users.noreply.github.com>
2023-04-16 20:23:46 +0530

    april_2nd_upload

---
+++ touch.sh
@@ -0,0 +1,9 @@
+git init
+git remote add origin https://github.com/yedotimahesh/Hospital_Management_System.git
+git remote -v  --force
+git add . --force
+git commit -m "april_2nd_upload"
+
+
+git push -f origin master
+
